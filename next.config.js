2cf983ccbbdb60eda34065f30eb19fcd610ce497
crohnaro <viniciosalfa@hotmail.com>
2022-07-22 18:25:54 -0300

    Primeira etapa concluida, todos requisitos concluidos

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+}*/
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/api',
+          destination: 'https://doar-computador-api.herokuapp.com/',
+        },
+      ]
+    },
+};
+
+
